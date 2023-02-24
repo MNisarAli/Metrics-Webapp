f22f5898bb2a7774ee404e97f92fd9cee46130d9
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-24 18:54:09 +0500

    Update App.js with routing logic for CountryDetails page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Continents from './pages/Continents';
 import Countries from './pages/Countries';
+import CountryDetails from './pages/CountryDetails';
 import './App.css';
 
 const App = () => (
@@ -11,6 +12,7 @@ const App = () => (
       <Routes>
         <Route path="/" exact element={<Continents />} />
         <Route index path="/continent" element={<Countries />} />
+        <Route index path="/country" element={<CountryDetails />} />
       </Routes>
     </main>
   </Router>
