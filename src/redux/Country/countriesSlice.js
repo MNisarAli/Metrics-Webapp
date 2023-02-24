d76a54dc9b267fcd18615f159bc52d6be5092513
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-24 13:25:47 +0500

    Add countries data fetching logic with async thunk

---
+++ src/redux/Country/countriesSlice.js
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const FETCH_COUNTRY_DATA = 'FETCH_COUNTRY_DATA';
+const COUNTRY_API_URL = 'https://restcountries.com/v3.1/region/';
+const COUNTRY_MAP_URL = 'https://raw.githubusercontent.com/djaiss/mapsicon/master/all/';
+
+const initialState = {
+  countries: [],
+  status: 'idle',
+  error: null,
+};
+
+// Transform the raw data fetched from the API into the desired format.
+const countryInformation = (data) => data
+  .filter((country) => country.independent)
+  .map((country) => ({
+    path: `/${country.name.common}`,
+    name: country.name.common,
+    official: country.name.official,
+    id: country.cca3,
+    capital: country.capital,
+    region: country.region,
+    subregion: country.subregion,
+    continent: country.continents[0],
+    population: country.population,
+    lat: country.latlng[0],
+    lng: country.latlng[1],
+    area: country.area,
+    flagSymbol: country.flag,
+    flag: country.flags.png,
+    map: `${COUNTRY_MAP_URL}${country.cca2.toLowerCase()}/vector.svg`,
+  })).sort((a, b) => a.name.localeCompare(b.name));
+
+// Async thunk to fetch countries by region and filter by name.
+export const getCountries = createAsyncThunk(
+  FETCH_COUNTRY_DATA,
+  async ({ region, name }) => {
+    const response = await axios.get(`${COUNTRY_API_URL}${region}`);
+    const filteredCountries = response.data.filter(
+      (country) => country.continents[0] === name,
+    );
+
+    return countryInformation(filteredCountries);
+  },
+);
+
+const countriesSlice = createSlice({
+  name: 'countries',
+  initialState,
+  reducers: {},
+  // Add extra reducers for the getCountries thunk.
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCountries.pending, (state) => ({
+        ...state,
+        status: 'loading',
+      }))
+      .addCase(getCountries.fulfilled, (state, action) => ({
+        ...state,
+        status: 'succeeded',
+        countries: action.payload,
+      }));
+  },
+});
+
+export default countriesSlice.reducer;
