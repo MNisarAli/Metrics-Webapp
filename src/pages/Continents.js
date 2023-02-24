cff70c5630762b23c3a1a15f486b26f7acd0fbaa
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-24 17:44:29 +0500

    Add navigation function for selected continent page

--- src/pages/Continents.js
+++ src/pages/Continents.js
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { getContinents } from '../redux/Continent/continentsSlice';
 import Dashboard from '../components/Dashboard';
 import Wrapper from '../components/Wrapper';
 
 const Continents = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { continents, status } = useSelector((state) => state.continents);
 
@@ -25,6 +27,11 @@ const Continents = () => {
 
   const worldMap = 'https://svgsilh.com/svg/306338.svg';
 
+  // Handle click event when a continent is clicked
+  const handleContinentClick = (continent) => {
+    navigate(`/continent?continent=${JSON.stringify(continent)}`);
+  };
+
   return (
     <>
       {/* Show dashboard with world map and total population */}
@@ -39,6 +46,10 @@ const Continents = () => {
               className="nav-item"
               key={continent.id}
               href={continent.path}
+              onClick={(e) => {
+                e.preventDefault();
+                handleContinentClick(continent);
+              }}
             >
               <Wrapper wrapper={continent} />
             </a>
