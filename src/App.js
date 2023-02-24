840bc34d4c8d02f0e18422d1b8448085f2c27d9d
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-24 17:49:19 +0500

    Update App.js with routing logic for Continents page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Continents from './pages/Continents';
 
 const App = () => (
-  <Header />
+  <Router>
+    <Header />
+    <main>
+      <Routes>
+        <Route path="/" exact element={<Continents />} />
+      </Routes>
+    </main>
+  </Router>
 );
 
 export default App;
